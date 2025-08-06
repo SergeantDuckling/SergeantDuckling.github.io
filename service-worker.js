97c2f56d7c9a2c87b1a01b4ddf244c359392483d
Jack Borthwick <Jac.Borthwick@CCG.VIC.EDU.AU>
2025-08-06 10:04:38 +1000

    Deploy with extra data inputs on speedtest.razor

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
+//Name - service-worker.js
+  //  Purpose - The page that litterally does nothing most of the time- occationally helps with the download
+//    last updated - 30/7/25
+//    author - Jack Borthwick-->
 const CACHE_NAME = 'irb-speed-cache-v1';
 self.addEventListener('install', evt => {
   evt.waitUntil(
